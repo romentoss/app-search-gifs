6f77b92d4f4a0c53f95e79e0ca6e7859abb4a7fe
Romentos <padilla.naranja@gmail.com>
2023-07-05 13:35:48 +0200

    last commit

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,9 @@ export class GifsService {
   private apiKey:string = 'vuQd8haSfC8Y2vrtMVGWFSXJk88BMU8z';
   private serviceUrl:string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http:HttpClient) { }
+  constructor( private http:HttpClient) {
+    this.loadLocalStorage();
+   }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -25,6 +27,24 @@ export class GifsService {
     }
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory))
+  }
+
+  private loadLocalStorage():void{
+    if(!localStorage.getItem('history')){
+      return;
+    }
+    this._tagsHistory =JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length === 0){
+      return;
+    }
+    this.searchTag(this._tagsHistory[0])
   }
 
   searchTag(tag:string):void{
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -7,21 +7,18 @@ import { GifsService } from 'src/app/gifs/services/gifs.service';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-
-
   constructor(private gifsService:GifsService){
-
-
   }
-
   get tags():string[]{
     return this.gifsService.tagsHistory;
   }
-
   searchTag(tag:string){
     this.gifsService.searchTag(tag);
   }
-
-
-
+  clearTag(tag:string, e:any){
+    e.stopPropagation();
+    console.log(this.gifsService.tagsHistory);
+    console.log(tag);
+    // this.gifsService.tagsHistory = this.gifsService.tagsHistory.filter(tag => tag === tag)
+  }
 }
